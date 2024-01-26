d5aa96c2f4b0aa5029dc6da5bcc9c42bb9a6fefb
Felipe Leão <felipeleao.tech@gmail.com>
2024-01-26 12:37:32 -0300

    :lipstick: feat: Estilização CSS todolist

--- scripts/menu-dropdown.js
+++ scripts/menu-dropdown.js
@@ -1,7 +1,30 @@
 export default function menuDropdown() {
-  const menuDrop = document.querySelector('.todolist__title-menu')
+  const menuDrop = document.querySelector(".todolist__title-menu");
 
-  menuDrop.addEventListener('click', () => {
-    console.log('oi')
-  })
+  menuDrop.addEventListener("click", handleClick);
+
+  function handleClick() {
+    this.classList.add("active");
+    outsideClick(this, () => {
+      this.classList.remove("active");
+    });
+  }
+
+  function outsideClick(element, callback) {
+    const html = document.documentElement;
+    const outside = "data-outside";
+
+    if (!element.hasAttribute(outside)) {
+      html.addEventListener("click", handleOutsideClick);
+      element.setAttribute(outside, "");
+    }
+
+    function handleOutsideClick(event) {
+      if (!element.contains(event.target)) {
+        element.removeAttribute(outside);
+        html.removeEventListener("click", handleOutsideClick);
+        callback();
+      }
+    }
+  }
 }
