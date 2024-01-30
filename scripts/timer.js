ca81711fde29d1b9b1c8758aad38b32e62a10bb4
Felipe Leão <felipeleao.tech@gmail.com>
2024-01-29 22:31:58 -0300

    att: timer

--- scripts/timer.js
+++ scripts/timer.js
@@ -1,9 +1,11 @@
 export default function timerCountdown() {
   const startBtn = document.querySelector(".pomodoro__controls-start");
   const displayBtns = document.querySelector(".pomodoro__controls-painel");
-  const playAndPause = document.querySelector(".btnPlayAndPause");
+  const playBtn = document.querySelector(".btnPlay");
+  const pauseBtn = document.querySelector(".btnPause");
   const stopBtn = document.querySelector(".btnStop");
-  const soundOnAndOff = document.querySelector(".soundOn");
+  const soundOnBtn = document.querySelector(".btnSoundOn");
+  const soundOffBtn = document.querySelector(".btnSoundOff");
 
   const timerDisplay = document.querySelector(".pomodoro__timer-display");
   const modeTime = document.querySelectorAll(".pomodoro__mode-title");
@@ -11,12 +13,17 @@ export default function timerCountdown() {
   const modeShort = document.querySelector(".modeShort");
   const modeLong = document.querySelector(".modeLong");
 
-  let tempoDecorrido = 2;
+  let tempoDecorrido = 1500;
   let countShort = 1;
   let countLong = 1;
   let timerCount = null;
+  let pomodoroOn = true;
+  let shortOn = null;
+  let longOn = null;
 
-  function showTimer(timeMode) {
+  // functions
+
+  function showTimer() {
     const time = new Date(tempoDecorrido * 1000);
     const formatTime = time.toLocaleTimeString("pt-Br", {
       minute: "2-digit",
@@ -35,11 +42,11 @@ export default function timerCountdown() {
   showTimer();
 
   function pomodoroTime() {
-    tempoDecorrido = 2;
+    tempoDecorrido = 1500;
   }
 
   function shortTime() {
-    tempoDecorrido = 3;
+    tempoDecorrido = 300;
   }
 
   function longTime() {
@@ -53,6 +60,11 @@ export default function timerCountdown() {
 
       if (tempoDecorrido == 0) {
         clearInterval(timerCount);
+        setTimeout(() => {
+          changeDisplayButtons();
+          resetDisplay(pauseBtn, playBtn);
+        }, 1000);
+
         isPomodoro();
         isShort();
         isLong();
@@ -68,6 +80,9 @@ export default function timerCountdown() {
         countLong++;
         countShort++;
         showTimer();
+        pomodoroOn = true;
+        shortOn = false;
+        longOn = false;
       }, 1000);
     }
   }
@@ -79,6 +94,9 @@ export default function timerCountdown() {
         modeChange(modeShort);
         countShort = 0;
         showTimer();
+        pomodoroOn = false;
+        shortOn = true;
+        longOn = false;
       }, 1000);
     }
   }
@@ -90,21 +108,65 @@ export default function timerCountdown() {
         modeChange(modeLong);
         countLong = 0;
         showTimer();
+        pomodoroOn = false;
+        shortOn = false;
+        longOn = true;
       }, 1000);
     }
   }
 
-  function changeDisplayButtons() {
-    startBtn.classList.add("hidden");
-    displayBtns.classList.remove("hidden");
+  function resetDisplay(element1, element2) {
+    element1.classList.remove("hidden");
+    element2.classList.add("hidden");
   }
 
-  function playAndPauseTimer() {}
+  function changeDisplayButtons() {
+    displayBtns.classList.toggle("hidden");
+    startBtn.classList.toggle("hidden");
+  }
+
+  function showAndHiddenBtn(element1, element2) {
+    element1.classList.toggle("hidden");
+    element2.classList.toggle("hidden");
+  }
 
   function stopTimer() {}
+
+  // eventos
 
   startBtn.addEventListener("click", () => {
     changeDisplayButtons();
     countdown();
+  });
+
+  playBtn.addEventListener("click", () => {
+    showAndHiddenBtn(pauseBtn, playBtn);
+    countdown();
+  });
+
+  pauseBtn.addEventListener("click", () => {
+    showAndHiddenBtn(pauseBtn, playBtn);
+    clearInterval(timerCount);
+  });
+
+  stopBtn.addEventListener("click", () => {
+    clearInterval(timerCount);
+    resetDisplay(playBtn, pauseBtn);
+    if (pomodoroOn) {
+      pomodoroTime();
+    } else if (shortOn) {
+      shortTime();
+    } else if (longOn) {
+      longTime();
+    }
+    showTimer();
+  });
+
+  soundOnBtn.addEventListener("click", () => {
+    showAndHiddenBtn(soundOnBtn, soundOffBtn);
+  });
+
+  soundOffBtn.addEventListener("click", () => {
+    showAndHiddenBtn(soundOnBtn, soundOffBtn);
   });
 }
