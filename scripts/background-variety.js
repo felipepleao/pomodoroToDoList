fe5994a329150b12d0ef413046df1e13a433179a
Felipe Leão <felipeleao.tech@gmail.com>
2024-02-13 20:49:08 -0300

    layout: backgrounds

---
+++ scripts/background-variety.js
@@ -0,0 +1,7 @@
+export default function varietyBackground() {
+  const modes = document.querySelectorAll(
+    ".main__container-backgrounds button"
+  );
+
+  console.log(modes);
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,11 @@
+import varietyBackground from "./background-variety.js";
 import menuDropdown from "./menu-dropdown.js";
 import timerCountdown from "./timer.js";
 import todoCrud from "./todolist-crud.js";
 
+varietyBackground();
 menuDropdown();
 timerCountdown();
-
 
 // var mainContainer = document.querySelector(".main__container");
 
