5d79ff859c01bf9ccfb93743a327c779a9c00b80
Felipe Leão <felipeleao.tech@gmail.com>
2024-02-14 23:26:55 -0300

    background: att

--- scripts/background-variety.js
+++ scripts/background-variety.js
@@ -7,13 +7,13 @@ export default function varietyBackground() {
   const btnBackground2 = document.querySelector(".btnBackground2");
   const btnBackground3 = document.querySelector(".btnBackground3");
 
-  function removeActive() {
+  function changeStatus() {
     modes.forEach((mode) => {
       mode.classList.remove("active");
     });
     this.classList.add("active");
 
-    let modeSelect = this.textContent;
+    let modeSelect = this.textContent.trim();
 
     switch (modeSelect) {
       case "Modo1":
@@ -30,7 +30,7 @@ export default function varietyBackground() {
     }
   }
 
-  btnBackground1.addEventListener("click", removeActive);
-  btnBackground2.addEventListener("click", removeActive);
-  btnBackground3.addEventListener("click", removeActive);
+  btnBackground1.addEventListener("click", changeStatus);
+  btnBackground2.addEventListener("click", changeStatus);
+  btnBackground3.addEventListener("click", changeStatus);
 }
